46c02029a63f0d653111b356072265b74d17a0c3
mhans19 <mdhansen19@gmail.com>
2020-07-11 23:50:13 -0600

    incorporated front-end fetch

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
-app.use(express.json());
+//MIDDLEWARE - always use when server deals with a front end and JSON data
+    // parse incoming string or array data
+    app.use(express.urlencoded({ extended: true }));
+    // parse incoming JSON data
+    app.use(express.json());
+    // get request for links in this folder (like css and js)
+    app.use(express.static('public'));
 
 //Functions
 function filterByQuery(query, animalsArray) {
@@ -114,7 +117,20 @@ app.post('/api/animals', (req, res) => {
     }
   });
 
+// Routes to serve 3 front-end html pages (order matters)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 //Port
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
-  });
+});
