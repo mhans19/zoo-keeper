73dfabb0641b496b16048e6d97aa01b9ae030d55
mhans19 <mdhansen19@gmail.com>
2020-07-12 00:31:31 -0600

    separate files and add zookeeper data with functioning

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const router = require('express').Router();
+
+// Routes to html files (front-end)
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+
+module.exports = router;
